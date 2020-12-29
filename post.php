85d350ccd3382509d42a1812567f55127f3e5830
Kristi <kristigylm@gmail.com>
2020-12-29 17:21:45 +0100

    switch chatid based on tracker

--- post.php
+++ post.php
@@ -4,13 +4,14 @@ include 'p_include.php';
 
 $ipinfo_token = '';
 $telegram_bot_token = '';
-$owner_telegram_id = 0
+
+// ?n=adv&country={country}&am={amount}&hit={hit_time}&sale={sale_time}&ip={ip}&tr={tracker}&to={chat_id}&switch=notrack%20{chat_id}%20k%20{chat_id}
 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-if (  !array_key_exists('n', $_REQUEST)
-   /*|| !array_key_exists('am', $_REQUEST)*/
-   && !array_key_exists('dont', $_REQUEST))
+if (  !array_key_exists('n', $_GET)
+   /*|| !array_key_exists('am', $_GET)*/
+   && !array_key_exists('dont', $_GET))
     {
         // Bye I have no duty here.
         echo 'Hello bye...';
@@ -19,24 +20,24 @@ if (  !array_key_exists('n', $_REQUEST)
 
 // ?n=adv&country={country}&am={amount}&hit={hit_time}&sale={sale_time}&ip={ip}&to=k
 
-if(isset($_REQUEST['hit'      ])) { $hit_time  = $_REQUEST['hit'     ]; } else { $hit_time  = '0'; }
-if(isset($_REQUEST['sale'     ])) { $sale_time = $_REQUEST['sale'    ]; } else { $sale_time = '0'; }
+if(isset($_GET['hit'      ])) { $hit_time  = $_GET['hit'     ]; } else { $hit_time  = '0'; }
+if(isset($_GET['sale'     ])) { $sale_time = $_GET['sale'    ]; } else { $sale_time = '0'; }
 
-if(isset($_REQUEST['ip'       ])) { $ip        = $_REQUEST['ip'      ]; } else { $ip        = ''; }
-if(isset($_REQUEST['am'       ])) { $amount    = $_REQUEST['am'      ]; } else { $amount    = '0'; }
-if(isset($_REQUEST['plat'     ])) { $platform  = $_REQUEST['plat'    ]; } else { $platform  = 'mobile'; }
-if(isset($_REQUEST['country'  ])) { $country   = $_REQUEST['country' ]; } else { $country   = ''; }
+if(isset($_GET['ip'       ])) { $ip        = $_GET['ip'      ]; } else { $ip        = ''; }
+if(isset($_GET['am'       ])) { $amount    = $_GET['am'      ]; } else { $amount    = '0'; }
+if(isset($_GET['plat'     ])) { $platform  = $_GET['plat'    ]; } else { $platform  = 'mobile'; }
+if(isset($_GET['country'  ])) { $country   = $_GET['country' ]; } else { $country   = ''; }
 
-if(isset($_REQUEST['t'/*time*/])) { $time2     = $_REQUEST['t'       ]; } else { $time2     = ''; }
-if(isset($_REQUEST['d'/*date*/])) { $date2     = $_REQUEST['d'       ]; } else { $date2     = ''; }
-if(isset($_REQUEST['s'/*src*/ ])) { $source    = $_REQUEST['s'       ]; } else { $source    = ''; }
-if(isset($_REQUEST['g'/*goal*/])) { $goal_id   = $_REQUEST['g'       ]; } else { $goal_id   = '0'; }
-if(isset($_REQUEST['o'/*offr*/])) { $offer     = $_REQUEST['o'       ]; } else { $offer     = ''; }
-if(isset($_REQUEST['n'/*netw*/])) { $network   = $_REQUEST['n'       ]; } else { $network   = 'adv'; }
+if(isset($_GET['t'/*time*/])) { $time2     = $_GET['t'       ]; } else { $time2     = ''; }
+if(isset($_GET['d'/*date*/])) { $date2     = $_GET['d'       ]; } else { $date2     = ''; }
+if(isset($_GET['s'/*src*/ ])) { $source    = $_GET['s'       ]; } else { $source    = ''; }
+if(isset($_GET['g'/*goal*/])) { $goal_id   = $_GET['g'       ]; } else { $goal_id   = '0'; }
+if(isset($_GET['o'/*offr*/])) { $offer     = $_GET['o'       ]; } else { $offer     = ''; }
+if(isset($_GET['n'/*netw*/])) { $network   = $_GET['n'       ]; } else { $network   = 'adv'; }
 
-if(isset($_REQUEST['tr'       ])) { $tracker   = $_REQUEST['tr'      ]; } else { $tracker   = ''; }
-if(isset($_REQUEST['s1'       ])) { $s1        = $_REQUEST['s1'      ]; } else { $s1        = ''; }
-if(isset($_REQUEST['s2'       ])) { $s2        = $_REQUEST['s2'      ]; } else { $s2        = ''; }
+if(isset($_GET['tr'       ])) { $tracker   = $_GET['tr'      ]; } else { $tracker   = 'notrack'; }
+if(isset($_GET['s1'       ])) { $s1        = $_GET['s1'      ]; } else { $s1        = ''; }
+if(isset($_GET['s2'       ])) { $s2        = $_GET['s2'      ]; } else { $s2        = ''; }
 
 
 $userip = $_SERVER['REMOTE_ADDR'];
@@ -88,17 +89,42 @@ else
      $shteti = code_to_country($country);
 }
 
-if(isset($_REQUEST['to']))
+if(isset($_GET['to']))
 {
-    $to = $_REQUEST['to'];
+    $to = $_GET['to'];
     
 }
-$reallysend = !array_key_exists('dont', $_REQUEST);
+$reallysend = !array_key_exists('dont', $_GET);
 
 
-// &to=k
-$to[0] == 'k' ? $chatid = $owner_telegram_id : $chatid = $to;
+// &to=owner
+$owner_telegram_id = 0
+$to[0] == 'owner' ? $chatid = $owner_telegram_id : $chatid = $to;
 
+// add &switch=notrack {chatid} track1 {chatid} track2 {chatid}
+// to switch chatid based on tracker 
+if(isset($_GET['switch']))
+{
+    $arrKeyValue = explode(" ", $_GET['switch']);
+    if(is_numeric($arrKeyValue[0]))
+    {
+        array_unshift($arrKeyValue, "notrack");
+    }
+    if(sizeof($arrKeyValue) % 2 != 0){ // key:value pairs are odd!
+        array_pop($arrKeyValue);
+    }
+
+    for ($i = 0; $i <= count($arrKeyValue); $i++) { // 
+        $k = $arrKeyValue[$i];
+        $v = $arrKeyValue[$i+1];
+        
+        if($tracker == $k){
+            $chatid = $v;
+            break;
+        }
+        $i++;
+    }
+}
 switch($network)
 {
     
